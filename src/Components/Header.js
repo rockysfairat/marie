729ca236aab61f1b1377c0bf111a12b92a5b82c5
Claudiu Ionel <ionel.claudiu394@gmail.com>
2024-03-20 15:33:28 +0100

    changed sizes in header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,22 +2,22 @@ import Image from "next/image";
 
 export function Header() {
   return (
-    <header className="w-full h-[90%] flex items-center justify-center  space-x-[90px] py-[80px]">
+    <header className="w-full h-[90%] flex items-center justify-center  space-x-[90px] py-[90px]">
       <Image
         src="/Header/C09.jpg"
-        width={400}
-        height={350}
+        width={650}
+        height={600}
         alt="Placeholder"
         style={{ objectFit: "cover" }}
-        className="ml-[100px] max-h-[350px]"
+        className=" max-h-[550px] max-w-[5600px]"
       />
 
-      <div className="relative flex flex-col w-2/5 justify-center h-[350px] ">
-        <p className="text-4xl text-balanced leading-snug uppercase max-w-[300px] font-light mt-[-80px] tracking-wide">
+      <div className="relative flex flex-col w-4/8 justify-center h-[550px] ">
+        <p className="text-5xl text-balanced leading-snug uppercase max-w-[400px] font-light mt-[-80px] tracking-wide">
           architecture interior design archviz
         </p>
         <a
-          className="absolute border-[1px] border-dark w-1/4 p-2 uppercase text-nowrap flex justify-center items-center bottom-0"
+          className="absolute border-[1px] border-dark w-2/4 p-2 uppercase text-nowrap flex justify-center items-center bottom-0"
           href="#"
         >
           Learn more
