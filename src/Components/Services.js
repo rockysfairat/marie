ef8b0c3d682df74b53284678564fc5a2a2f47e03
Ålexander Yurchenko <yurchenko@post.com>
2024-04-14 16:28:08 +0200

    Finished mobile version of Services

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -3,21 +3,21 @@ import { services } from "@/utils/data";
 
 export function Services() {
   return (
-    <div className="flex flex-col justify-start items-center bg-greyLight px-24">
+    <div className="flex flex-col justify-start items-center bg-greyLight px-2 xl:px-24 ">
       <h2 className="w-full">Services</h2>
-      <div className="w-[90%] lg:w-full flex lg:flex-row flex-col justify-between items-start lg:[&>*]:w-[22%]">
+      <div className="w-min xl:w-full flex lg:flex-row flex-col justify-between items-start xl:[&>*]:w-[22%] flex-wrap overflow-hidden">
         {services.map(({ id, serviceImgSrc, serviceImgAlt, serviceName }) => (
           <div key={id} className="flex flex-col mb-3">
             <p className="text-6xl text-brown font-light">{id}</p>
 
-            <div className="flex justify-center items-center my-2 h-[300px] w-full overflow-hidden">
+            <div className="flex justify-center items-center my-2 h-[300px] w-[300px] overflow-hidden">
               <Image
                 src={serviceImgSrc}
                 alt={serviceImgAlt}
                 width={300}
                 height={300}
                 style={{ objectFit: "cover" }}
-                className="h-[400px] w-[400px]"
+                className="h-[300px] w-[300px]"
               />
             </div>
 
